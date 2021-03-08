92f18298c7eadb2d76fa1c539020a5fdcaab1a21
teslaji <kbhatt385@gmailcom>
2021-03-08 16:41:16 +0530

    Added Montserrat Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 module.exports = {
-    prefix: '',
-    purge: {
-      enabled: true,
-      content: [
-        './src/**/*.{html,ts}',
-      ]
-    },
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/forms'),require('@tailwindcss/line-clamp'),require('@tailwindcss/typography')],
+  prefix: "",
+  purge: {
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: { fontfamily: { 'montserrat': ["Montserrat"] } },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography")
+  ],
 };
